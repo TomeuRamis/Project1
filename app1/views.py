775b5db8635a2e5d3e4ffc96d77af95fabf834ec
TomeuRamis <tomeuramis99@yahoo.es>
2018-12-13 20:08:20 +0100

    Finished check_processes with a more optimal algorithm

--- app1/views.py
+++ app1/views.py
@@ -3,12 +3,18 @@ from .forms import ProcessType
 from .models import Request, User
 import json
 import os
+import logging
 
 
 absPath = os.getcwd()
 pathReq = absPath + "ProcessManager/Files/Requests/"
 pathInProgress = absPath + "ProcessManager/Files/InProgress/"
 pathFinish = absPath + "ProcessManager/Files/Finished/"
+
+logging.basicConfig(filename='process_manager.log',
+                    level=logging.INFO,
+                    format='[%(asctime)s] %(levelname)s: %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
 # Create your views here.
@@ -26,15 +32,21 @@ def login(request):
                 request.session['email'] = user.email
                 context = {'user': user,
                            'access': True}
+                logging.info('User: ' + user.user_name + ' has logged in.')
                 return render(request, "app1/login.html", context)
             else:
+                logging.error('User: ' + user.user_name + ' has tried to log in with'
+                                                          ' a wrong email ( ' + request.POST['email'] + ' ).')
                 return render(request, "app1/loginFailed.html")
         except User.DoesNotExist:
+            logging.error('There was an attempt to log in with the wrong credentials '
+                          'user name: ' + request.POST['username'] + ', email: ' + request.POST['email'])
             return render(request, "app1/loginFailed.html")
     else:
         return render(request, "app1/login.html")
 
 
+# Load home page and update Request database
 def logged(request):
 
     user = User.objects.get(user_name=request.session['username'])
@@ -55,7 +67,7 @@ def requestprocess(request):
             r = Request(type_of_process=type_of_process, user=user)
             r.save()
             # Create the new file to store the Request
-            f = open(pathReq + "request" + str(r.id), "w+")
+            f = open(pathReq + request.session['username'] + "/request" + str(r.id), "w+")
             json.dump({"id": r.id,
                        "type": type_of_process,
                        "date of creation": str(r.date_of_creation),
@@ -63,8 +75,11 @@ def requestprocess(request):
                        "started": False,
                        "finished": False}, f)
             f.close()
+            logging.info("User: "+user.user_name+" has requested a new process"
+                                                 " [id:"+r.id+", type: "+type_of_process)
             return render(request, "app1/requestSuccessful.html", {'user': user})
         else:
+            logging.fatal("User: " + user.user_name + " tried to request a process but was an error validating the form")
             form = ProcessType()
             return render(request, "app1/requestProcess.html", {'form': form,
                                                                 'error': True,
@@ -89,22 +104,45 @@ def request_successful(request):
                           {'user': user})
 
 
-# Look at the Files folder to update the user's requests
+# Method that takes all of the user's requests and looks for upates at the Files folder.
+# First checks requests that have been not started and searches them in the InProgress folder,
+# the remaining processes that have started are searched in the Finished folder,
+# and latsly, searches started and not finished requests in the Finished folder.
 def check_processes(user):
+    user_path = user['user_name'] + '/'
+
     try:
-        for i, file in enumerate(os.listdir(pathInProgress)):
-            f = open(pathInProgress + file, 'r')
-            try:
-                request = json.load(f)
-                database_request = Request.objets.filter(user=user).filter(pk=request['id'])
-                if not database_request['started']:
-                    database_request.update(started=True)
+        # Get all processes that have not been started yet, and compare them with
+        # the Files/InProgress folder to update the database
+        database_requests = Request.objets.filter(user=user).filter(started=False)
+        for db_req in enumerate(database_requests):
+            for file in enumerate(os.listdir(pathInProgress + user_path)):
+                f = open(pathInProgress + user_path + file, 'r')
+                req = json.load(f)
+                if db_req['pk'] == req['id']:
+                    logging.info("Process with id: "+req['id']+" was updated to: started=True")
+                    db_req.update(started=True)
 
-            except Request.DoesNotExist:
-                print("The process with id: " + i + " was not found on the Data Base")
+        # Get the remaining database-processes that haven't started, and check if they have already finished
+        database_requests = Request.objets.filter(user=user).filter(started=False)
+        for db_req in enumerate(database_requests):
+            for file in enumerate(os.listdir(pathFinish + user_path)):
+                f = open(pathInProgress + user_path + file, 'r')
+                req = json.load(f)
+                if db_req['pk'] == req['id']:
+                    logging.info("Process with id: "+req['id']+" was updated to: finished=True")
+                    db_req.update(finished=True)
 
-        for i, file in enumerate(os.listdir((pathFinish))):
-            f = open()
+        # Get all processes that have been started but have not finished, and updates them
+        database_requests = Request.objets.filter(user=user).filter(started=True).filter(finished=False)
+        for db_req in enumerate(database_requests):
+            for file in enumerate(os.listdir(pathFinish + user_path)):
+                f = open(pathInProgress + user_path + file, 'r')
+                req = json.load(f)
+                if db_req['pk'] == req['id']:
+                    logging.info("Process with id: "+req['id']+" was updated to: finished=True")
+                    db_req.update(finished=True)
 
     except IOError:
+        logging.fatal("There was a I/O problem updating the processes of User: " + user.user_name)
         print("I/O error")
